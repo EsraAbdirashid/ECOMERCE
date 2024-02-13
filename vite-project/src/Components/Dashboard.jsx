bd6af30fe2bd92f70ab49e55e3c27fca4d1d265c
EsraAbdirashid <israApdirashid@gmail.com>
2024-02-13 17:54:26 +0300

    done

--- vite-project/src/Components/Dashboard.jsx
+++ vite-project/src/Components/Dashboard.jsx
@@ -3,12 +3,21 @@ import React from 'react'
 const Dashboard = () => {
   return (
     <div>
-     <div className="Navbar1 bg-yellow-700 ">
+     <div className="Navbar1 bg-yellow-700 flex justify-between p-2 pl-4 pr-4 ">
      <div className="logo">
         <h2>ZooEcomerce</h2>
       </div>
-      <div className="components">
-        
+      <div className="components flex pr-16">
+        <div className="search">
+        <input type="text"placeholder='search' />
+        <button>🔍</button>
+        </div>
+        <div className="cart">
+        <i class='bx bx-cart-alt'></i>
+        </div>
+        <div className="dark_light">
+        <i class='bx bx-sun'></i>
+        </div>
       </div>
      </div>
     </div>
