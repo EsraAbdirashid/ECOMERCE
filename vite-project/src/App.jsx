3d2896aeca57833c74961128e96acb3c9980821b
EsraAbdirashid <israApdirashid@gmail.com>
2024-02-13 17:18:44 +0300

    Done

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className='bg-red-600 text-white'>
+    <div className='bg-red-600 text-white text-center text-2xl'>
       Hello Hamda
     </div>
   )
