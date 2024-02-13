f597fd85bc28db7a17d9fd4fd24deb6d2790d1f3
EsraAbdirashid <israApdirashid@gmail.com>
2024-02-13 17:39:32 +0300

    done

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Dashboard from './Components/Dashboard'
 
 const App = () => {
   return (
     <div >
-     
+     <Dashboard/>
     </div>
   )
 }
---
+++ vite-project/src/Components/Dashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div>
+        <h2 className='bg-pink-600 text-white text-center'>Hello Miss Hamda</h2>
+    </div>
+  )
+}
+
+export default Dashboard
