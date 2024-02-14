ec8a59e0fe098cb3bda5ed587693e3f68375a90b
EsraAbdirashid <israApdirashid@gmail.com>
2024-02-14 18:51:53 +0300

    done

--- vite-project/src/Components/Dashboard.jsx
+++ vite-project/src/Components/Dashboard.jsx
@@ -50,10 +50,10 @@ const Dashboard = () => {
      </div>
 
      <div className="Home flex justify-center p-[100px]">
-        <div className="text_button">
+        <div className="text_button ">
         <h1 className='text-white text-6xl font-semibold '>30% off on All <br /> Women's Wear</h1>
-        <p className='text-white text-start'>who's there Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla quaerat aliquam <br /> dolorem natus quia, blanditiis similique vel quibusdam rem perferendis in repellendus optio <br /> doloremque  eveniet provident, dolorum nemo ea laudantium.</p>
-        <button className='bg-pink-600 px-4 font-medium p-1 text-white rounded-full hover:bg-pink-800 '>Order Now</button>
+        <p className='text-white text-start mt-4'>who's there Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla quaerat aliquam  dolorem natus  quia, blanditiis similique    vel <br /> quibusdam rem perferendis in repellendus optio  doloremque  eveniet <br /> provident, dolorum nemo ea laudantium.</p>
+        <button className='bg-pink-600 px-4 font-medium p-1 text-white rounded-full hover:bg-pink-800 mt-4 '>Order Now</button>
         </div>
         <div className="image">
                 <img src="./Public/image1.png" alt="" className='w-[1000px] -mt-24 -pr-7' />
