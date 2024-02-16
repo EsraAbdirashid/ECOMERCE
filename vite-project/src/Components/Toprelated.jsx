85e63cb9702d05d6aa711ec19ffd8ddcffd4b477
EsraAbdirashid <israApdirashid@gmail.com>
2024-02-16 19:45:24 +0300

    done

--- vite-project/src/Components/Toprelated.jsx
+++ vite-project/src/Components/Toprelated.jsx
@@ -3,14 +3,14 @@ import React from 'react'
 const Toprelated = () => {
   return (
     <div>
-          <div className="text ">
+          <div className="text justify-center">
             <h6>Top Selling products for you</h6>
             <h1>Top SElling Products</h1>
             <p>Clothes ipsum dolor sit amet consectetur adipisicing elit. Quidem id quis !</p>
           </div>
           <div className="img flex gap-7">
             <img src="./public/img1.jpg" alt="" className='w-[180px] h-[180px] rounded-md' />
-            <img src="./public/img22.jpg" alt="" className='w-[180px] h-[180px] rounded-md' />
+          
             <img src="./public/img3.jpg" alt="" className='w-[180px] h-[180px] rounded-md' />
             <img src="./public/img44.jpg" alt="" className='w-[180px] h-[180px] rounded-md' />
             <img src="./public/img5.jpg" alt="" className='w-[180px] h-[180px] rounded-md' />
