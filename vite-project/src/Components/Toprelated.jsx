46f4e640b9cb1a4d3580091508be4d34de7e9c9b
EsraAbdirashid <israApdirashid@gmail.com>
2024-02-16 19:43:33 +0300

    done

--- vite-project/src/Components/Toprelated.jsx
+++ vite-project/src/Components/Toprelated.jsx
@@ -10,9 +10,9 @@ const Toprelated = () => {
           </div>
           <div className="img flex gap-7">
             <img src="./public/img1.jpg" alt="" className='w-[180px] h-[180px] rounded-md' />
-            <img src="./public/img2.jpg" alt="" className='w-[180px] h-[180px] rounded-md' />
+            <img src="./public/img22.jpg" alt="" className='w-[180px] h-[180px] rounded-md' />
             <img src="./public/img3.jpg" alt="" className='w-[180px] h-[180px] rounded-md' />
-            <img src="./public/img4.jpg" alt="" className='w-[180px] h-[180px] rounded-md' />
+            <img src="./public/img44.jpg" alt="" className='w-[180px] h-[180px] rounded-md' />
             <img src="./public/img5.jpg" alt="" className='w-[180px] h-[180px] rounded-md' />
             <img src="./public/img6.jpg" alt="" className='w-[180px] h-[180px] rounded-md' />
           </div>
