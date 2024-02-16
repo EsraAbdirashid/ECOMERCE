59b6cb1c7bc53200c8143b8dcd39f09e0483ec6e
EsraAbdirashid <israApdirashid@gmail.com>
2024-02-16 20:11:32 +0300

    done

--- vite-project/src/Components/Toprelated.jsx
+++ vite-project/src/Components/Toprelated.jsx
@@ -58,7 +58,7 @@ const Toprelated = () => {
             </div>
           </div>
           <div className="button text-center mt-7">
-            <button className='bg-pink-600 text-white px-9 font-medium rounded-lg p-2'>View All Products</button>
+            <button className='bg-pink-600 text-white px-9 font-medium rounded-lg p-2 hover:bg-pink-800 '>View All Products</button>
           </div>
     </div>
   )
