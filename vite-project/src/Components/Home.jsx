e4b717b91f585df39ae72b53b1b3f3ee3ede85e9
EsraAbdirashid <israApdirashid@gmail.com>
2024-02-16 18:31:38 +0300

    done

--- vite-project/src/Components/Home.jsx
+++ vite-project/src/Components/Home.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <div>
-       <div className="Home bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 flex justify-center p-[100px]">
+    <div> 
+      <div className='h-[300px] w-[300px] bg-indigo-700 absolute -z-9  right-0 rounded-3xl rotate-45  '>
+
+      </div>
+       {/* <div className="Home bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 flex justify-center p-[100px]">
+
         <div className="text_button ">
         <h1 className='text-white text-6xl font-semibold '>30% off on All <br /> Women's Wear</h1>
         <p className='text-white text-start mt-4'>who's there Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla quaerat aliquam  dolorem natus  quia, blanditiis similique    vel <br /> quibusdam rem perferendis in repellendus optio  doloremque  eveniet <br /> provident, dolorum nemo ea laudantium.</p>
@@ -12,7 +16,7 @@ const Home = () => {
         <div className="image">
                 <img src="./Public/image1.png" alt="" className='w-[1100px] -mt-20' />
         </div>
-     </div>
+     </div> */}
     </div>
   )
 }
