b3649834a0bf7f5acf6418e8b25570f3aa2ca44b
EsraAbdirashid <israApdirashid@gmail.com>
2024-02-17 05:51:06 +0300

    Done

--- vite-project/src/Components/Dashboard.jsx
+++ vite-project/src/Components/Dashboard.jsx
@@ -127,14 +127,14 @@ const Dashboard = () => {
                         <h1 className='text-4xl font-bold mt-2'>Top Rated Products</h1>
                         <p className='text-slate-400 mt-2'>to rated products ipsum dolor, sit amet consectetur adipisicing elit.</p>
                         <div className="imges flex items-center justify-center mt-5">
-                               <div className="img1">
+                               <div className="img1 box-border w-[300px] h-[300px]">
                                <img src="./public/shirt1.jpg" alt="" className='w-[200px] h-[200px]' />
                                <FaStar className='text-orange-600 inline-block'/> 
                                <FaStar className='text-orange-600 inline-block'/> 
                                <FaStar className='text-orange-600 inline-block'/> 
                                <FaStar className='text-orange-600 inline-block'/> 
                                <h3>Casual Wear</h3>
-                               <p> repellat rem, laboriosam dolorum sapiente possimus ab soluta vel architecto libero, numquam voluptates? Exercitationem, reprehenderit distinctio. Soluta, magnam.</p>
+                               <p> repellat rem, laboriosam dolorum sapiente possimus ab soluta vel architecto libero, </p>
                                <button>Order now</button>
                                </div>
                                 <img src="./public/shirt2.jpg" alt="" className='w-[200px] h-[200px]' />
