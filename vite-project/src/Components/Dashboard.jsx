aec7ce07b95650a3dc8750d1e0d39d2855070c7b
EsraAbdirashid <israApdirashid@gmail.com>
2024-02-17 05:31:51 +0300

    Done

--- vite-project/src/Components/Dashboard.jsx
+++ vite-project/src/Components/Dashboard.jsx
@@ -125,7 +125,7 @@ const Dashboard = () => {
                         <h6>Trending products</h6>
                         <h1>Top Rated Products</h1>
                         <p>to rated products ipsum dolor, sit amet consectetur adipisicing elit.</p>
-                        <div className="imges">
+                        <div className="imges flex">
                                 <img src="./public/shirt1.jpg" alt="" />
                                 <img src="./public/shirt2.jpg" alt="" />
                                 <img src="./public/shirt3.jpg" alt="" />
