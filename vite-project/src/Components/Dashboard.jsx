62fc547cfe0d8cb58db33082ae345ae3738dd8fe
EsraAbdirashid <israApdirashid@gmail.com>
2024-02-19 23:44:08 +0300

    Done

--- vite-project/src/Components/Dashboard.jsx
+++ vite-project/src/Components/Dashboard.jsx
@@ -265,7 +265,7 @@ const Dashboard = () => {
           </div>
           {/* mens */}
           <div className="Mens mt-4 bg-gradient-to-r from-pink-500 via-purple-500 to-indigo-500 h-screen flex justify-center m-auto text-center p-7">
-        <div className="text_button ">
+        <div className="text_button translate-x-9 ">
         <h1 className='text-white text-6xl font-semibold '>30% off on All <br /> Men's Wear</h1>
         <p className='text-white text-start mt-4'>who's there Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla quaerat aliquam  dolorem natus  quia, blanditiis similique    vel <br /> quibusdam rem perferendis in repellendus optio  doloremque  eveniet <br /> provident, dolorum nemo ea laudantium.</p>
         <button className='bg-pink-600 px-4 font-medium p-1 text-white rounded-full hover:bg-pink-800 mt-4 '>Order Now</button>
