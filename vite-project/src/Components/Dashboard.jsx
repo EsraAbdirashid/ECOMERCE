13b100815347aaba859e0718ab59ac304e402652
EsraAbdirashid <israApdirashid@gmail.com>
2024-02-19 23:48:59 +0300

    Done

--- vite-project/src/Components/Dashboard.jsx
+++ vite-project/src/Components/Dashboard.jsx
@@ -264,14 +264,14 @@ const Dashboard = () => {
 
           </div>
           {/* mens */}
-          <div className="Mens mt-4 bg-gradient-to-r from-pink-500 via-purple-500 to-indigo-500 h-screen flex justify-center m-auto text-center p-7">
-        <div className="text_button translate-x-9 ">
+          <div className="Mens mt-4 bg-gradient-to-r from-pink-500 via-purple-500 to-indigo-500 h-screen flex p-7">
+        <div className="text_button ml-28 mt-24">
         <h1 className='text-white text-6xl font-semibold '>30% off on All <br /> Men's Wear</h1>
         <p className='text-white text-start mt-4'>who's there Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla quaerat aliquam  dolorem natus  quia, blanditiis similique    vel <br /> quibusdam rem perferendis in repellendus optio  doloremque  eveniet <br /> provident, dolorum nemo ea laudantium.</p>
         <button className='bg-pink-600 px-4 font-medium p-1 text-white rounded-full hover:bg-pink-800 mt-4 '>Order Now</button>
         </div>
-        <div className="image">
-                <img src="./Public/man.png" alt="" className='w-[1700px] h-[500px]' />
+        <div className="image ml-[50px]">
+                <img src="./Public/man.png" alt="" className='w-[700px] h-[500px]' />
         </div>
      </div>
 
