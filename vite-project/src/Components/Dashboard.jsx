dc0a3b69c2b7f15cb13f89f0c17068fd6faba6f3
EsraAbdirashid <israApdirashid@gmail.com>
2024-02-19 23:51:24 +0300

    Done

--- vite-project/src/Components/Dashboard.jsx
+++ vite-project/src/Components/Dashboard.jsx
@@ -268,7 +268,7 @@ const Dashboard = () => {
         <div className="text_button ml-28 mt-20">
         <h1 className='text-white text-6xl font-semibold '>30% off on All <br /> Men's Wear</h1>
         <p className='text-white text-start mt-7'>who's there Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla quaerat aliquam  dolorem natus  quia, blanditiis similique    vel <br /> quibusdam rem perferendis in repellendus optio  doloremque  eveniet <br /> provident, dolorum nemo ea laudantium.</p>
-        <button className='bg-pink-600 px-4 font-medium p-1 text-white rounded-full hover:bg-pink-800 mt-4 '>Order Now</button>
+        <button className='bg-pink-800 px-4 font-medium p-1 text-white rounded-full hover:bg-indigo-950 mt-9 '>Order Now</button>
         </div>
         <div className="image ml-[50px] mt-7">
                 <img src="./Public/man.png" alt="" className='w-[700px] h-[500px]' />
