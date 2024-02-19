d98c791d5a2e9c1cd28ebf8a01e5e687834f583e
EsraAbdirashid <israApdirashid@gmail.com>
2024-02-19 19:29:33 +0300

    Done

--- vite-project/src/Components/Dashboard.jsx
+++ vite-project/src/Components/Dashboard.jsx
@@ -222,7 +222,7 @@ const Dashboard = () => {
             </div>
 
             <div className="img2">
-            <img src="./public/kids2.jpg" alt="" className='w-[180px] h-[180px] rounded-md' />
+            <img src="./public/kids22.jpg" alt="" className='w-[180px] h-[180px] rounded-md' />
             <h3 className='font-bold text-pink-700 mt-4'>Women Western</h3>
             <span>Red</span>
             <div className="icon">
@@ -240,7 +240,7 @@ const Dashboard = () => {
             </div>
             </div>
             <div className="img4">
-            <img src="./public/kids4.jpg" alt="" className='w-[180px] h-[180px] rounded-md' />
+            <img src="./public/kids44.jpg" alt="" className='w-[180px] h-[180px] rounded-md' />
             <h3 className='font-bold text-pink-700 mt-4'>Printed T-shirt</h3>
             <span>Yellow</span>
             <div className="icon">
