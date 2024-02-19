57f4b99c97b16210e4f477ca591e7c7dc3c2ea2e
EsraAbdirashid <israApdirashid@gmail.com>
2024-02-19 23:54:28 +0300

    Done

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,29 +1,29 @@
-// import React from 'react'
-// import Dashboard from './Components/Dashboard'
-// import { Outlet } from 'react-router-dom'
-// // import Home from './Components/Home'
-// import Toprelated from './Components/Toprelated'
-// import MensWear from './Components/MensWear'
+import React from 'react'
+import Dashboard from './Components/Dashboard'
+import { Outlet } from 'react-router-dom'
+// import Home from './Components/Home'
+import Toprelated from './Components/Toprelated'
+import MensWear from './Components/MensWear'
 
-// const App = () => {
-//   return (
-//     <div >
-//      <Dashboard/>
-//      <Outlet />
+const App = () => {
+  return (
+    <div >
+     <Dashboard/>
+     <Outlet />
 
-//      {/* <main> */}
-//       {/* <div id='Home'>
-//        <Home/>
-//       </div> */}
-//       {/* <div id='Toprelated'>
-//     <Toprelated/>
-//       </div>
-//       <div id='MensWear'>
-//         <MensWear/>
-//       </div>
-//      </main> */}
-//     </div>
-//   )
-// }
+      <main> 
+     <div id='Home'>
+       <Home/>
+      </div> 
+     <div id='Toprelated'>
+    <Toprelated/>
+      </div>
+      <div id='MensWear'>
+        <MensWear/>
+      </div>
+     </main> 
+    </div>
+  )
+}
 
-// export default App
+export default App
