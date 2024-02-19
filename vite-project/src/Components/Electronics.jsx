2ca9e731acb63fd68dfe2792a07e50ea035e80ab
EsraAbdirashid <israApdirashid@gmail.com>
2024-02-19 23:55:40 +0300

    Done

--- vite-project/src/Components/Electronics.jsx
+++ vite-project/src/Components/Electronics.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+// import React from 'react'
 
-const Electronics = () => {
-  return (
-    <div>Electronics</div>
-  )
-}
+// const Electronics = () => {
+//   return (
+//     <div>Electronics</div>
+//   )
+// }
 
-export default Electronics
+// export default Electronics
