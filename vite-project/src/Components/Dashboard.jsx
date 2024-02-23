6ce92ede8d63c56055d88b15587ba97fdcf9aeb2
EsraAbdirashid <israApdirashid@gmail.com>
2024-02-23 19:07:35 +0300

    DOne

--- vite-project/src/Components/Dashboard.jsx
+++ vite-project/src/Components/Dashboard.jsx
@@ -339,7 +339,7 @@ const Dashboard = () => {
             <button className='bg-indigo-600 text-white px-9 font-medium rounded-lg p-2 hover:bg-pink-800 '>View All Products</button>
           </div>
           {/* testimonial */}
-          <div className="Testimonial">
+          <div className="Testimonial ">
           <div className="text">
             <h6>What our customers say!</h6>
             <h1>Testimonial</h1>
@@ -347,10 +347,14 @@ const Dashboard = () => {
           </div>
           <div className="boxes">
             <div className="box1 bg-blue-400 w-[300px] h-[300px]">
-             <img src="./public/customer1.jpg" alt="" className='w-[100px] h-[100px]' />
+             <img src="./public/customer1.jpg" alt="" className='w-[80px] h-[80px] rounded-full' />
             </div>
           </div>
           </div>
+
+
+          <div className="">hhhhhhhhhhkkkkkkkkllllllll
+          sdjsfdjhf</div>
 
     </div>
   )
